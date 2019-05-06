291a0eb06f916cbe0c9a262770805cb61b820e05
Philip Campani <toycampani@gmail.com>
2019-05-06 09:54:13 -0300

    Simple CSS reset

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "./Header/Header";
+import "../styles/style.scss";
 
 function App() {
   return (
--- app/components/Header/Nav.js
+++ app/components/Header/Nav.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = styled.header`
-  font-family: Helvetica, sans-serif;
   display: grid;
 
   nav,
